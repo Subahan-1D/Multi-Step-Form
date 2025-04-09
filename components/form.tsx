e26909498f9ad64107de94aced1c392377dfd133
subahan ali <subahanislam523@gmail.com>
2025-04-09 09:22:33 +0600

    added to by personal information or state Adress

--- components/form.tsx
+++ components/form.tsx
@@ -14,7 +14,7 @@ const steps = [
   {
     id: 'Step 1',
     name: 'Personal Information',
-    fields: ['firstName', 'lastName', 'email' , 'phoneNumber']
+    fields: ['firstName', 'lastName', 'email', 'phoneNumber']
   },
   {
     id: 'Step 2',
@@ -167,7 +167,7 @@ export default function Form() {
                 </div>
               </div>
 
-              <div className='sm:col-span-4'>
+              <div className='sm:col-span-3'>
                 <label
                   htmlFor='email'
                   className='block text-sm font-medium leading-6 text-gray-900'
@@ -185,6 +185,28 @@ export default function Form() {
                   {errors.email?.message && (
                     <p className='mt-2 text-sm text-red-400'>
                       {errors.email.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className='sm:col-span-3'>
+                <label
+                  htmlFor='phoneNumber'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  phoneNumber
+                </label>
+                <div  className='mt-2'>
+                  <input
+                    id='phoneNumber'
+                    type='tel'
+                    {...register('phoneNumber')}
+                    autoComplete='tel'
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6'
+                  />
+                  {errors.phoneNumber?.message && (
+                    <p className='mt-2 text-sm text-red-400'>
+                      {errors.phoneNumber.message}
                     </p>
                   )}
                 </div>
@@ -221,9 +243,9 @@ export default function Form() {
                     autoComplete='country-name'
                     className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:max-w-xs sm:text-sm sm:leading-6'
                   >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
+                    <option>Bangladesh</option>
+                    <option>India</option>
+                    <option>China</option>
                   </select>
                   {errors.country?.message && (
                     <p className='mt-2 text-sm text-red-400'>
@@ -274,29 +296,6 @@ export default function Form() {
                   {errors.city?.message && (
                     <p className='mt-2 text-sm text-red-400'>
                       {errors.city.message}
-                    </p>
-                  )}
-                </div>
-              </div>
-
-              <div className='sm:col-span-2'>
-                <label
-                  htmlFor='state'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
-                  State / Province
-                </label>
-                <div className='mt-2'>
-                  <input
-                    type='text'
-                    id='state'
-                    {...register('state')}
-                    autoComplete='address-level1'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6'
-                  />
-                  {errors.state?.message && (
-                    <p className='mt-2 text-sm text-red-400'>
-                      {errors.state.message}
                     </p>
                   )}
                 </div>
